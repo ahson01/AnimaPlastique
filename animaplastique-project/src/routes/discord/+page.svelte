ae8d83924a039340e6d7cfe6f6d9124970d86bba
ahson01 <itsahson1978@gmail.com>
2025-11-28 01:21:03 +0530

    added discord

---
+++ animaplastique-project/src/routes/discord/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Discord · AnimaPlastique</title>
+</svelte:head>
+
+
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		window.location.href = 'https://discord.gg/9kNaxhtHXM';
+	});
+</script>
+
