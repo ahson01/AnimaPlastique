bcf4e05dae8ae0957a8b253334a5eee253c1af51
Ahson01 <ahson01@proton.me>
2026-02-06 11:34:31 +0530

    another attempt at fixing geo prices

--- animaplastique-project/src/lib/server/geolocation.ts
+++ animaplastique-project/src/lib/server/geolocation.ts
@@ -8,16 +8,29 @@ export const getCountryFromIp = (ip: string) => {
 };
 
 export const getCurrencyForCountry = (countryCode: string) => {
-    const currencyMap: Record<string, string> = {
-        IN: 'INR',
-        US: 'USD',
-        GB: 'GBP',
-        EU: 'EUR',
-        CA: 'CAD',
-        // Add more mappings as needed
-    };
-
     // Default to USD for unknown countries outside India
     // But since our default country is IN, it will be INR for unknown IPs.
     return currencyMap[countryCode] || (countryCode === 'IN' ? 'INR' : 'USD');
+};
+
+const currencyMap: Record<string, string> = {
+    // North America
+    US: 'USD', CA: 'CAD', MX: 'MXN',
+    // Europe
+    EU: 'EUR', GB: 'GBP', DE: 'EUR', FR: 'EUR', IT: 'EUR', ES: 'EUR', NL: 'EUR', BE: 'EUR',
+    AT: 'EUR', PT: 'EUR', GR: 'EUR', FI: 'EUR', IE: 'EUR', LU: 'EUR', CY: 'EUR', EE: 'EUR',
+    LV: 'EUR', LT: 'EUR', MT: 'EUR', SK: 'EUR', SI: 'EUR', BG: 'BGN', HR: 'HRK', CZ: 'CZK',
+    DK: 'DKK', HU: 'HUF', PL: 'PLN', RO: 'RON', SE: 'SEK', CH: 'CHF', NO: 'NOK', IS: 'ISK',
+    LI: 'CHF', RS: 'RSD', BA: 'BAM', MK: 'MKD', AL: 'ALL',
+    // Asia
+    JP: 'JPY', CN: 'CNY', IN: 'INR', HK: 'HKD', TW: 'TWD', KR: 'KRW', SG: 'SGD', ID: 'IDR',
+    MY: 'MYR', TH: 'THB', VN: 'VND', PH: 'PHP', PK: 'PKR', BD: 'BDT', LK: 'LKR', NP: 'NPR',
+    AE: 'AED', SA: 'SAR', QA: 'QAR', KW: 'KWD', OM: 'OMR', BH: 'BHD', IL: 'ILS', TR: 'TRY',
+    RU: 'RUB', KZ: 'KZT', UZ: 'UZS',
+    // Oceania
+    AU: 'AUD', NZ: 'NZD',
+    // South America
+    BR: 'BRL', AR: 'ARS', CL: 'CLP', CO: 'COP', PE: 'PEN', UY: 'UYU',
+    // Africa
+    ZA: 'ZAR', EG: 'EGP', NG: 'NGN', KE: 'KES', MA: 'MAD',
 };
